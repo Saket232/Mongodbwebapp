9350db2e91ae4d529cd250f438e3ebead7a96eb4
Saket232 <45016030+Saket232@users.noreply.github.com>
2020-09-13 18:33:29 +0530

    Add files via upload

---
+++ src/test/java/com/patient/spiringbootmongo/SpiringbootmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patient.spiringbootmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpiringbootmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
